352138a5ffd231c73ecc3f53966e68960230b725
unknown <gshaidar6@gmail.com>
2023-03-19 19:54:42 +0700

    perbaikan sedikit pada halaman all pokemon dan menambah animasi sederhana serta menambah warna pada card berdasarkan typepokemon

--- src/components/PokemonCard .jsx
+++ src/components/PokemonCard .jsx
@@ -2,22 +2,24 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./assets/css/component.css";
 
-const PokemonCard = ({x}) => {
-    console.log(x.data.types[0].type.name)
+const PokemonCard = ({ x }) => {
+  console.log(x.data.types[0].type.name);
   return (
-    <div className="col-lg-3">
-        <div className=" card my-2" key={x.id}>
-      <Link to={`/allpokemon/${x.id}`} className={`text-decoration-none pokemon-card ${x.data.types[0].type.name}`}>
-        <div className="m-3 d-flex text-center justify-content-center">
-          <div>
-            <img src={x.data.sprites.front_default} alt={x.name} />
-            <p className="text-white">{x.name}</p>
+    <div className="PokemonCard col-lg-3">
+      <div className=" card my-3 bayangan" key={x.id}>
+        <Link
+          to={`/allpokemon/${x.id}`}
+          className={`text-decoration-none pokemon-card ${x.data.types[0].type.name}`}
+        >
+          <div className="m-3 d-flex text-center justify-content-center">
+            <div>
+              <img src={x.data.sprites.front_default} alt={x.name} />
+              <p className="text-white">{x.name}</p>
+            </div>
           </div>
-        </div>
-      </Link>
+        </Link>
+      </div>
     </div>
-    </div>
-    
   );
 };
 
